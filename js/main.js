bdc2f57bfa48735265404d99398aa33272958dac
Bartek <bartosz.majewski03szkola@gmail.com>
2025-05-18 00:59:04 +0200

    w pelni dzialajace wyszukiwanie

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 const search_input = document.querySelector("#search_input");
+const selected_category = document.querySelector("#category");
+const search_button = document.querySelector("#search_button");
 const subpages_list_container = document.querySelector("#subpages_list");
+
 let subpages = [];
 
 fetch('../db.json')
@@ -11,12 +14,24 @@ fetch('../db.json')
         console.log(error);
     });
 
-function testClick(){
-    subpages_list_container.innerHTML="";
-    subpages.forEach(subpage => {
-    const li = document.createElement("li");
-    li.innerHTML=`${subpage.id} - ${subpage.name} - ${subpage.character} - ${subpage.category} - ${subpage.url}`;
-    
-    subpages_list_container.appendChild(li);
+function search(){
+    const input_value = search_input.value.toLowerCase();
+    const category = selected_category.value;
+    let isFound = false;
+    subpages_list_container.innerHTML = '';
+    subpages.forEach(item => {
+        const category_match = item.category == category || category == "wszystko";
+        if(category_match && item.character.includes(input_value))
+        {
+            isFound = true;
+            const subpage = document.createElement("li");
+            subpage.innerHTML = `<a href="${item.url}">${item.character}</a>`;
+            subpages_list_container.appendChild(subpage);
+        }
     });
+    if(!isFound){
+        const subpage = document.createElement("li");
+        subpage.innerHTML = 'Nie znaleziono wyników';;
+        subpages_list_container.appendChild(subpage);
+    }
 }
