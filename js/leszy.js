6350c00e890a7f73659fdd3b6ccb1e54bc28afa7
Oliwier Góralczyk <artorias.gws@gmail.com>
2025-05-23 15:41:56 +0200

    hover w js

--- js/leszy.js
+++ js/leszy.js
@@ -1,13 +1,16 @@
-function toggleClassesOnClick(imageSelector, containerSelector, imageClass1, imageClass2, containerClass1, containerClass2) {
+function toggleClassesOnClick(imageSelector, containerSelector, imageClass1, imageClass2, containerClass1, containerClass2) 
+{
     const image = document.querySelector(imageSelector);
     const container = document.querySelector(containerSelector);
 
-    if (image==null || container==null) {
+    if (image==null || container==null) 
+    {
         console.log("Nie znaleziono jednego z elementów:", imageSelector, containerSelector);
         return;
     }
 
-    image.addEventListener("click", function () {
+    image.addEventListener("click", function () 
+    {
         image.classList.toggle(imageClass1);
         image.classList.toggle(imageClass2);
         container.classList.toggle(containerClass1);
@@ -16,3 +19,16 @@ function toggleClassesOnClick(imageSelector, containerSelector, imageClass1, ima
 }
 
 toggleClassesOnClick("#zdjecie-leszego", "#kontener-leszego", "zdjecie-male", "zdjecie-duze", "kontener-maly", "kontener-duzy");
+
+document.addEventListener("DOMContentLoaded", function () {
+    const hoverText = document.querySelector(".hovertx");
+    const hoverImg = hoverText.querySelector(".hoverimg");
+
+    hoverText.addEventListener("mouseenter", function () {
+        hoverImg.style.display = "block";
+    });
+
+    hoverText.addEventListener("mouseleave", function () {
+        hoverImg.style.display = "none";
+    });
+});
