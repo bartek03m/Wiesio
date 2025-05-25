f718b5892d21cb70c824a5bc124cb04b9cb5b98f
Oliwier Góralczyk <artorias.gws@gmail.com>
2025-05-25 23:16:30 +0200

    dodanie slidera?

---
+++ js/slider_krabopajonk.js
@@ -0,0 +1,40 @@
+let zdj=[];
+let aktualne_zdj=0;
+const slajd=document.querySelector("#slide");
+const podpis=document.querySelector("#podpis-slider")
+fetch('/data/slider.json')
+    .then(response => response.json())
+    .then(data => {
+        zdj=data[slajd.dataset.slider];
+        pokazSlajd(0);
+    })
+    .catch(error => {
+        console.warn("nie udalo się zaladować slidera", error);
+    });
+function pokazSlajd(idx){
+    console.log(idx)
+    
+    if (idx<0){
+        aktualne_zdj=zdj.length-1;
+    }
+    else if (idx>zdj.length-1){
+        aktualne_zdj=0;
+    }
+    else{
+        aktualne_zdj=idx;
+    }
+    slajd.src=zdj[aktualne_zdj];
+}
+document.querySelector("#poprzednie").addEventListener("click",function(){
+    pokazSlajd(aktualne_zdj-1);
+});
+document.querySelector("#nastepne").addEventListener("click",function(){
+    pokazSlajd(aktualne_zdj+1);
+});
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        pokazSlajd(aktualne_zdj - 1);
+    } else if (e.key === "ArrowRight") {
+        pokazSlajd(aktualne_zdj + 1);
+    }
+});
