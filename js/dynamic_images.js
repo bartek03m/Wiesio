6a57fdc21f63b5ebaedc93351d84bdaefb97cf41
Oliwier Góralczyk <artorias.gws@gmail.com>
2025-05-25 02:18:24 +0200

    Duże zmiany, aktualizacja wyglądu i działania

---
+++ js/dynamic_images.js
@@ -0,0 +1,49 @@
+function ToggleImgClass(imageSelector, containerSelector, imageClass1, imageClass2, containerClass1, containerClass2) 
+{
+    const image = document.querySelector(imageSelector);
+    const container = document.querySelector(containerSelector);
+
+    if (image==null || container==null) 
+    {
+        console.log("Nie znaleziono jednego z elementów:", imageSelector, containerSelector);
+        return;
+    }
+
+    image.addEventListener("click", function () 
+    {
+        image.classList.toggle(imageClass1);
+        image.classList.toggle(imageClass2);
+        container.classList.toggle(containerClass1);
+        container.classList.toggle(containerClass2);
+    });
+}
+
+function ImgHover(textSelector, imageSelector)
+{
+    const texts = document.querySelectorAll(textSelector);
+    const imgs = document.querySelectorAll(imageSelector);
+
+    if (texts.length !== imgs.length) {
+        console.warn('ImgHover: różna liczba tekstów i obrazków.');
+        return;
+    }
+    if (!texts || !imgs) {
+        console.warn('ImgHover: nie znaleziono elementów tekstowych lub obrazkowych.');
+        return;
+    }
+
+    texts.forEach((textEl, idx) => 
+    {
+        const imgEl = imgs[idx];
+
+        textEl.addEventListener('mouseenter', function() 
+        {
+            imgEl.style.display = 'block';
+        });
+        
+        textEl.addEventListener('mouseleave', function() 
+        {
+            imgEl.style.display = 'none';
+        });
+    });
+}
