7072b411d068178221e07044c3c82d4d2cb99511
Bartek <bartosz.majewski03szkola@gmail.com>
2025-05-25 23:38:19 +0200

    final commit

--- js/spoiler.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const spoilers = document.querySelectorAll('.spoiler');
-
-  spoilers.forEach(spoiler => {
-    spoiler.addEventListener('click', () => {
-      spoiler.classList.toggle('revealed');
-    });
-  });
-});
