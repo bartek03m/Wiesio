3979c4d12e73ce339451cdb83d7b06772522de6c
Bartek <bartosz.majewski03szkola@gmail.com>
2025-06-06 16:55:30 +0200

    quizpo

---
+++ js/quizpo.js
@@ -0,0 +1,75 @@
+const pytanieBox = document.querySelector("#pytanie");
+const A = document.querySelector("#a");
+const B = document.querySelector("#b");
+const C = document.querySelector("#c");
+const D = document.querySelector("#d");
+let aktualnePytanie = 0;
+let punkty = 0;
+let liczbaPytan;
+
+fetch('../data/quiz.json')
+    .then(response => response.json())
+    .then(data => {
+        const quiz = data.po;
+        liczbaPytan = Object.keys(quiz).length;
+        wczytajPytanie(quiz);
+        A.addEventListener("click", () => {sprawdzOdpowiedz(quiz, 0)});
+        B.addEventListener("click", () => {sprawdzOdpowiedz(quiz, 1)});
+        C.addEventListener("click", () => {sprawdzOdpowiedz(quiz, 2)});;
+        D.addEventListener("click", () => {sprawdzOdpowiedz(quiz, 3)});
+    })
+    .catch(error => {
+        console.warn("nie udało się załadować pytania", error);
+    });
+
+function wczytajPytanie(quiz) 
+{
+    const pytanieTresc = quiz[aktualnePytanie].pytanie;
+    const odpowiedzi = quiz[aktualnePytanie].odpowiedzi;
+    
+    pytanieBox.innerHTML = pytanieTresc;
+    A.innerHTML = "A. " + odpowiedzi[0];
+    B.innerHTML = "B. " + odpowiedzi[1];
+    C.innerHTML = "C. " + odpowiedzi[2];
+    D.innerHTML = "D. " + odpowiedzi[3];
+}
+
+function sprawdzOdpowiedz(quiz, odpowiedz) 
+{
+    const poprawnaOdpowiedz = quiz[aktualnePytanie].poprawna_odpowiedz;
+    if (odpowiedz == poprawnaOdpowiedz) 
+    {
+        punkty++;
+        alert("Dobra odpowiedź!");
+    } 
+    else 
+    {
+        alert(`Zła odpowiedź! Poprawna to: ${quiz[aktualnePytanie].odpowiedzi[poprawnaOdpowiedz]}`);
+    }
+    aktualnePytanie++;
+    if(aktualnePytanie < quiz.length) {
+        wczytajPytanie(quiz);
+    }
+    else{
+        wyswietlWynik();
+    }
+}
+
+function wyswietlWynik() 
+{
+    let HS = localStorage.getItem("highscorePO");
+    if (HS == null || punkty > HS) 
+    {
+        HS = punkty;
+        localStorage.setItem("highscorePO", HS);
+    }
+    pytanieBox.innerHTML = `Koniec quizu! Zdobyłeś ${punkty}/${liczbaPytan} punktów.<br>Twój rekord to: ${HS} punktów.`;
+    A.innerHTML = "";
+    B.innerHTML = "";
+    C.innerHTML = "";
+    D.innerHTML = "";
+    A.style.display = "none";
+    B.style.display = "none";
+    C.style.display = "none";
+    D.style.display = "none";
+}
